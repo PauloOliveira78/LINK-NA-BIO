cf440a2a6529b8a28cf3a6f94ff7743ec16cd993
PauloOliveira78 <paulo.oliveira78@outlook.com>
2024-12-13 14:15:09 -0300

    Ajustando estrutura e revisando codigo

--- js/script.js
+++ js/script.js
@@ -1,41 +1,31 @@
-function toggleTheme() {
-  const root = document.documentElement;
-  const toggleContainer = document.getElementById("theme-toggle-container");
+document.addEventListener("DOMContentLoaded", function () {
+  const themeToggleButton = document.getElementById("theme-toggle")
+  const body = document.body
 
-  if (root) {
-    root.classList.toggle("light");
-  }
-
-  if (toggleContainer) {
-    if (root.classList.contains("light")) {
-      toggleContainer.classList.add("light");
+  // Função para definir a classe do dispositivo
+  function setDeviceClass() {
+    if (window.innerWidth <= 768) {
+      body.classList.add("mobile")
+      body.classList.remove("desktop")
     } else {
-      toggleContainer.classList.remove("light");
+      body.classList.add("desktop")
+      body.classList.remove("mobile")
     }
   }
-}
 
-// Verifica o estado do tema ao carregar a página
-function initializeTheme() {
-  const root = document.documentElement;
-  const savedTheme = localStorage.getItem("theme");
+  // Definir a classe do dispositivo ao carregar a página
+  setDeviceClass()
 
-  if (savedTheme) {
-    root.classList.toggle("light", savedTheme === "light");
-    const toggleContainer = document.getElementById("theme-toggle-container");
-    if (toggleContainer) {
-      toggleContainer.classList.toggle("light", savedTheme === "light");
+  // Atualizar a classe do dispositivo ao redimensionar a janela
+  window.addEventListener("resize", setDeviceClass)
+
+  themeToggleButton.addEventListener("click", function () {
+    if (body.classList.contains("root")) {
+      body.classList.remove("root")
+      body.classList.add("light")
+    } else {
+      body.classList.remove("light")
+      body.classList.add("root")
     }
-  }
-}
-
-// Define o tema ao clicar no botão
-document.getElementById("theme-toggle").addEventListener("click", () => {
-  toggleTheme();
-  // Armazena o estado do tema no localStorage
-  const root = document.documentElement;
-  localStorage.setItem("theme", root.classList.contains("light") ? "light" : "dark");
-});
-
-// Inicializa o tema ao carregar a página
-initializeTheme();
+  })
+})
